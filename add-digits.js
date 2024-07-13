cad1add42e132a1830aa38ccb424e4fb52c87d76
Andrew <andrew.lizon12@gmail.com>
2024-07-13 10:55:20 -0700

    Completed addDigits

---
+++ add-digits.js
@@ -0,0 +1,14 @@
+var addDigits = function(num) {
+    // We are given a number
+    // If the number has more than two digits, then add its digits
+    // else return it
+
+
+    if (num < 10) return num
+    
+    else {
+        num = String(num).split("")
+        num = num.reduce((a,c) => a + Number(c), 0)
+        return addDigits(num)
+    }
+};
