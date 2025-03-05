84169761c56324a4689db1484172d3d733cc95a0
Andrew <andrew.lizon12@gmail.com>
2025-03-05 14:47:16 -0800

    Added new solution to merge sorted arrays

--- Leetcode_JS/merge-sorted-arrays.js
+++ Leetcode_JS/merge-sorted-arrays.js
@@ -11,3 +11,28 @@ var merge = function(nums1, m, nums2, n) {
         nums1.sort((a,b) => a-b)
 
 };
+
+// Alternative Solution
+var merge = function(nums1, m, nums2, n) {
+    // Compare the elements in nums2 to elements in nums1
+    // if an element is greater, then push to end of nums1 
+    if (n === 0) return
+        let len1 = nums1.length
+        let endIndex = len1 - 1
+        
+        while (n > 0 && m > 0) {
+            if (nums2[n-1] >= nums1[m-1]) {
+                nums1[endIndex] = nums2[n-1]
+                n--
+            } else {
+                nums1[endIndex] = nums1[m-1]
+                m--
+            }
+            endIndex--
+        }
+        while (n>0) {
+            nums1[endIndex] = nums2[n-1]
+            n--
+            endIndex--
+        }
+};
