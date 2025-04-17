b6235abf3eedf6b9595f9bc0dfe9e8bac1e146e3
Andrew <andrew.lizon12@gmail.com>
2025-04-16 21:35:26 -0700

    Completed is subsequence PY

---
+++ Leetcode_Python/is-subsequence.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        spointer = 0
+        tpointer = 0
+
+        while spointer < len(s) and tpointer < len(t):
+            if s[spointer] is t[tpointer]:
+                spointer += 1
+            else:
+                tpointer +=1
+        return spointer == len(s)
