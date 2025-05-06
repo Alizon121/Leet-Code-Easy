f153b019a9e8510cb88da20f9c6972d47e29c5fc
Andrew <andrew.lizon12@gmail.com>
2025-05-06 10:39:25 -0700

    String Compression PY

---
+++ Leetcode_Python/string-compression.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+
+        n = len(chars)
+        idx = 0
+        i = 0
+
+        while i < n:
+            ch = chars[i]
+            count = 0
+            while i < n and chars[i] == ch:
+                i+=1
+                count+=1 # Will count the number of same values in a row 
+            if count == 1:
+                chars[idx] = ch
+                idx+=1
+            else:
+                chars[idx] = ch
+                idx+=1
+                for num in str(count):
+                    chars[idx] = num
+                    idx+=1
+        chars[:] = chars[:idx]
+        return idx
