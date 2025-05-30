40c8109ab2bb637ae003521ff6847bb723e586eb
Andrew <andrew.lizon12@gmail.com>
2025-05-29 19:23:36 -0700

    Completed Determine if Two Strings Are Close PY

---
+++ Leetcode_Python/determine-two-strings-close.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def closeStrings(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: bool
+        """
+        
+        if len(word1) != len(word2):
+            return False
+
+        freq1 = Counter(word1)
+        freq2 = Counter(word2)
+
+        if set(freq1.keys()) != set(freq2.keys()):
+            return False
+
+        return sorted(freq1.values()) == sorted(freq2.values())
