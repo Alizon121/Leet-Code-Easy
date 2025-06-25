20852899a91aa18452f3869b5cd587e8dc689b8e
Andrew <andrew.lizon12@gmail.com>
2025-06-24 20:28:16 -0700

    Refactor search-BST.ts

--- Leetcode_TS/search-BST.ts
+++ Leetcode_TS/search-BST.ts
@@ -1,3 +1,15 @@
+class TreeNode {
+     val: number
+     left: TreeNode | null
+     right: TreeNode | null
+     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+         this.val = (val===undefined ? 0 : val)
+         this.left = (left===undefined ? null : left)
+         this.right = (right===undefined ? null : right)
+     }
+ }
+
+
 function searchBSTt(root: TreeNode | null, val: number): TreeNode | null {
     if (!root) return null
 
